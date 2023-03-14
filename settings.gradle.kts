2ea65176cd3925cb7cd6290b5b5a635b5201c215
GitHub Action <action@github.com>
2023-03-14 08:35:14 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "golang-ut-gen"
--- src/main/kotlin/com/github/dougsong/golangutgen/MyBundle.kt
+++ src/main/kotlin/com/github/dougsong/golangutgen/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.dougsong.golangutgen
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
